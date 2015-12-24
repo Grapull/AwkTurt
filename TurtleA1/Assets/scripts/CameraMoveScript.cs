dda9181b56ee4dae5f6d946570e4787aa86d72d0
TheLastJoel <joelbearn@gmail.com>
2015-12-24 08:19:47 +0000

    Created Project
    Created the unity project and added assets from pong

---
+++ TurtleA1/Assets/scripts/CameraMoveScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoveScript : MonoBehaviour {
+	public GameObject cameraToMove;
+	public GameObject Turtle;
+	private Camera cam;
+	public float Ypos;
+	// Use this for initialization
+	void Start () {
+		Ypos = Turtle.transform.position.y;
+		cam = GetComponent<Camera> ();
+		cam.orthographicSize = 4;
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+		cameraToMove.transform.position = 	new Vector3 (Turtle.transform.position.x,Ypos-0,-10);
+	}
+
+}
